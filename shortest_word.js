c0bef10b1b98e9176164dd1e84e94af80a4a5901
Crew Spence <csspence123@gmail.com>
2020-01-14 06:40:26 -0600

    Created function stub for Code Wars toy problem.

---
+++ shortest_word.js
@@ -0,0 +1,11 @@
+/*
+Simple, given a string of words, return the length of the shortest word(s).
+
+String will never be empty and you do not need to account for different data types.
+*/
+
+//Restate the problem in your own words.
+//What are the inputs?
+//What are the outputs?
+//Can the outputs be determined from the inputs?  Do I already have enough information to solve the problem?  What if you don't have enough information from the user?
+//How should you label the important pieces of data that are a part of the problem?
