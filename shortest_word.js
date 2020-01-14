24cef60e7a96ba2f36b9339b93ad9ec0a72aa83e
Crew Spence <csspence123@gmail.com>
2020-01-14 07:15:39 -0600

    Wrote working solution for shortest_word.

--- shortest_word.js
+++ shortest_word.js
@@ -9,3 +9,15 @@ String will never be empty and you do not need to account for different data typ
 //What are the outputs?
 //Can the outputs be determined from the inputs?  Do I already have enough information to solve the problem?  What if you don't have enough information from the user?
 //How should you label the important pieces of data that are a part of the problem?
+
+const findShort = (str) => {
+    let arr = str.split(" ");
+    let answer = str.length;
+    for(let i = 0; i < arr.length; i++) {
+      if(arr[i].length < answer) {
+        answer = arr[i].length;
+      }
+    }
+  
+    return answer;
+  }
